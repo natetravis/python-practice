efb4eb3bd5a3c7012b3d69d444ab723260ea325d
Travis <travisdanny10@gmail.com>
2024-12-18 18:55:01 +0300

    ...

---
+++ python/test.py
@@ -0,0 +1,7 @@
+import math_operations
+from math_operations import addition, subtraction #importing specific modules
+from math_operations import multiplication as mul #importing with an alias
+import math_operations as mo #alias to the whole module
+from math_operations import * #import all functions
+
+print(math_operations.addition(2, 5))
